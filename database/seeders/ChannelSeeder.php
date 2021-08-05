ddfba823cea63c5c56df16adc9a1da29aba231dd
mohamed <mohammed2571994@gmail.com>
2021-08-05 08:55:08 +0300

    change seeders names

--- database/seeders/ChannelSeeder.php
+++ database/seeders/ChannelSeeder.php
@@ -22,25 +22,25 @@ class ChannelSeeder extends Seeder
             'current_team_id' => '1',
         ]);
 
-        $channel2 = User::create([
-            'name' => "mohamed",
-            'email' => 'mohamed@email.com',
+        User::create([
+            'name' => "user",
+            'email' => 'user@email.com',
             'password' => bcrypt('11112222'),
-            'administration_level' => '0',
+            'administration_level' => '1',
             'current_team_id' => '1',
         ]);
 
-        $channel1 = User::create([
-            'name' => "mohamed1",
-            'email' => 'mohamed1@email.com',
+        User::create([
+            'name' => "user1",
+            'email' => 'user1@email.com',
             'password' => bcrypt('11112222'),
             'administration_level' => '2',
             'current_team_id' => '1',
         ]);
 
-        $channel1 = User::create([
-            'name' => "mohamed2",
-            'email' => 'mohamed2@email.com',
+        User::create([
+            'name' => "user2",
+            'email' => 'user2@email.com',
             'password' => bcrypt('11112222'),
             'administration_level' => '2',
             'current_team_id' => '1',
--- database/seeders/ConvertedVideoSeeder.php
+++ database/seeders/ConvertedVideoSeeder.php
@@ -19,11 +19,11 @@ class ConvertedVideoSeeder extends Seeder
         foreach ($videos as $video) {
             Convertedvideo::create([
                 'video_id' => $video->id,
-                'mp4_Format_240' => '240p-7a2yZUhyq7oKH4hQ.mp4',
-                'mp4_Format_360' => '360p-7a2yZUhyq7oKH4hQ.mp4',
-                'mp4_Format_480' => '480p-XG4XBR9Y4wTGWNUv.mp4',
-                'mp4_Format_720' => '720p-i2vuNWIDioSjDrla.mp4',
-                'mp4_Format_1080' => '1080p-XG4XBR9Y4wTGWNUv.mp4',
+                'mp4_Format_240' => '240p-video.mp4',
+                'mp4_Format_360' => '360p-video.mp4',
+                'mp4_Format_480' => '480p-video.mp4',
+                'mp4_Format_720' => '720p-video.mp4',
+                'mp4_Format_1080' => '1080p-video.mp4',
                 'webm_Format_240'=>'No Video',
                 'webm_Format_360'=>'No Video',
                 'webm_Format_480'=>'No Video',
